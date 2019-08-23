4f4c685a784f676dc86c3a7bf348751326935312
LyoSU <ua.lyo.su@gmail.com>
2019-08-23 16:23:39 +0300

    0.3.9

--- handlers/sticker-delete.js
+++ handlers/sticker-delete.js
@@ -2,25 +2,29 @@ const Markup = require('telegraf/markup')
 
 
 module.exports = async (ctx) => {
-  const setName = ctx.callbackQuery.message.reply_to_message.sticker.set_name
-  const packBotUsername = setName.split('_').pop(-1)
+  let packBotUsername
+  let deleteSticker
+
+  if (ctx.callbackQuery.message.reply_to_message.sticker) {
+    const setName = ctx.callbackQuery.message.reply_to_message.sticker.set_name
+
+    packBotUsername = setName.split('_').pop(-1)
+  }
 
   if (!ctx.session.user) ctx.session.user = await ctx.db.User.getData(ctx.from)
   const sticker = await ctx.db.Sticker.findOne({
     fileId: ctx.match[2],
   }).populate('stickerSet')
 
-  let deleteSticker
-
   if (sticker && sticker.stickerSet.owner.toString() === ctx.session.user.id.toString()) {
-    deleteSticker = sticker.info
+    deleteSticker = sticker.info.file_id
   }
-  else if (packBotUsername === ctx.options.username) {
-    deleteSticker = ctx.callbackQuery.message.reply_to_message.sticker
+  else if (packBotUsername && packBotUsername === ctx.options.username) {
+    deleteSticker = ctx.callbackQuery.message.reply_to_message.sticker.file_id
   }
 
   if (deleteSticker) {
-    const deleteStickerFromSet = await ctx.deleteStickerFromSet(deleteSticker.file_id).catch((error) => {
+    const deleteStickerFromSet = await ctx.deleteStickerFromSet(deleteSticker).catch((error) => {
       ctx.answerCbQuery(ctx.i18n.t('error.answerCbQuery.telegram', {
         error: error.description,
       }), true)
@@ -31,7 +35,7 @@ module.exports = async (ctx) => {
 
       ctx.editMessageText(ctx.i18n.t('callback.sticker.delete'), {
         reply_markup: Markup.inlineKeyboard([
-          Markup.callbackButton(ctx.i18n.t('callback.sticker.btn.restore'), `restore_sticker:${deleteSticker.file_id}`),
+          Markup.callbackButton(ctx.i18n.t('callback.sticker.btn.restore'), `restore_sticker:${deleteSticker}`),
         ]),
       }).catch(() => {})
 
