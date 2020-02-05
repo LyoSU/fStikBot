3e2532c055d23746ebbabe62ee4909d1d0313285
LyoSU <mail@lyo.su>
2020-02-05 18:29:03 +0200

    0.4.3 fix db

--- database/index.js
+++ database/index.js
@@ -2,7 +2,9 @@ const collections = require('./models')
 const connection = require('./connection')
 
 
-const db = {}
+const db = {
+  connection,
+}
 
 Object.keys(collections).forEach((collectionName) => {
   db[collectionName] = connection.model(collectionName, collections[collectionName])
