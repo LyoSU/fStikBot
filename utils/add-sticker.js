dbfb08f22c67717bc26c3cd0d7d85d603b588963
LyoSU <mail@lyo.su>
2020-03-06 23:33:13 +0200

    0.4.19

--- utils/add-sticker.js
+++ utils/add-sticker.js
@@ -19,116 +19,119 @@ const downloadFileByUrl = (fileUrl) => new Promise((resolve, reject) => {
 })
 
 module.exports = async (ctx, inputFile) => {
-  let stickerFile = inputFile
+  const tmpPath = `tmp/${inputFile.file_id}_${Date.now()}.png`
 
-  const originalSticker = await ctx.db.Sticker.findOne({
-    fileUniqueId: stickerFile.file_unique_id
-  })
+  const result = await (async () => {
+    let stickerFile = inputFile
 
-  if (originalSticker && originalSticker.file) stickerFile = originalSticker.file
+    const originalSticker = await ctx.db.Sticker.findOne({
+      fileUniqueId: stickerFile.file_unique_id
+    })
 
-  if (!ctx.session.user) ctx.session.user = await ctx.db.User.getData(ctx.from)
+    if (originalSticker && originalSticker.file) stickerFile = originalSticker.file
 
-  const nameSuffix = `_by_${ctx.options.username}`
-  const titleSuffix = ` by @${ctx.options.username}`
+    if (!ctx.session.user) ctx.session.user = await ctx.db.User.getData(ctx.from)
 
-  const defaultStickerSet = {
-    owner: ctx.session.user.id,
-    name: `f_${Math.random().toString(36).substring(5)}_${ctx.from.id}`,
-    title: 'Favorite stickers',
-    emojiSuffix: '🌟'
-  }
+    const nameSuffix = `_by_${ctx.options.username}`
+    const titleSuffix = ` by @${ctx.options.username}`
 
-  defaultStickerSet.name += nameSuffix
-  if (ctx.session.user.premium !== true) defaultStickerSet.title += titleSuffix
-  if (!ctx.session.user.stickerSet) ctx.session.user.stickerSet = await ctx.db.StickerSet.getSet(defaultStickerSet)
-
-  let emojis = inputFile.emoji || ''
-
-  emojis += ctx.session.user.stickerSet.emojiSuffix || ''
-
-  if (stickerFile.is_animated !== true) {
-    const fileUrl = await ctx.telegram.getFileLink(stickerFile)
-    const data = await downloadFileByUrl(fileUrl)
-    const imageSharp = sharp(data.read())
-    const imageMetadata = await imageSharp.metadata().catch(() => {})
-
-    if (
-      imageMetadata.width > 512 || imageMetadata.height > 512 ||
-      (imageMetadata.width !== 512 && imageMetadata.height !== 512)
-    ) {
-      if (imageMetadata.height > imageMetadata.width) imageSharp.resize({ height: 512 })
-      else imageSharp.resize({ width: 512 })
+    const defaultStickerSet = {
+      owner: ctx.session.user.id,
+      name: `f_${Math.random().toString(36).substring(5)}_${ctx.from.id}`,
+      title: 'Favorite stickers',
+      emojiSuffix: '🌟'
     }
 
-    const tmpPath = `tmp/${stickerFile.file_id}_${Date.now()}.png`
+    defaultStickerSet.name += nameSuffix
+    if (ctx.session.user.premium !== true) defaultStickerSet.title += titleSuffix
+    if (!ctx.session.user.stickerSet) ctx.session.user.stickerSet = await ctx.db.StickerSet.getSet(defaultStickerSet)
 
-    await imageSharp.webp({ quality: 100 }).png({ force: false }).toFile(tmpPath).catch(() => {})
+    let emojis = inputFile.emoji || ''
 
-    const hash = await hasha.fromFile(tmpPath, { algorithm: 'md5' }).catch(() => {})
+    emojis += ctx.session.user.stickerSet.emojiSuffix || ''
 
-    let stickerAdd = false
+    if (stickerFile.is_animated !== true) {
+      const fileUrl = await ctx.telegram.getFileLink(stickerFile)
+      const data = await downloadFileByUrl(fileUrl)
+      const imageSharp = sharp(data.read())
+      const imageMetadata = await imageSharp.metadata().catch(() => {})
 
-    if (ctx.session.user.stickerSet.create === false) {
-    // eslint-disable-next-line max-len
-      stickerAdd = await ctx.telegram.createNewStickerSet(ctx.from.id, ctx.session.user.stickerSet.name, ctx.session.user.stickerSet.title, {
-        png_sticker: { source: tmpPath },
-        emojis
-      }).catch((error) => {
-        return {
-          error: {
-            telegram: error
+      if (
+        imageMetadata.width > 512 || imageMetadata.height > 512 ||
+        (imageMetadata.width !== 512 && imageMetadata.height !== 512)
+      ) {
+        if (imageMetadata.height > imageMetadata.width) imageSharp.resize({ height: 512 })
+        else imageSharp.resize({ width: 512 })
+      }
+
+      await imageSharp.webp({ quality: 100 }).png({ force: false }).toFile(tmpPath).catch(() => {})
+
+      const hash = await hasha.fromFile(tmpPath, { algorithm: 'md5' }).catch(() => {})
+
+      let stickerAdd = false
+
+      if (ctx.session.user.stickerSet.create === false) {
+        stickerAdd = await ctx.telegram.createNewStickerSet(ctx.from.id, ctx.session.user.stickerSet.name, ctx.session.user.stickerSet.title, {
+          png_sticker: { source: tmpPath },
+          emojis
+        }).catch((error) => {
+          return {
+            error: {
+              telegram: error
+            }
           }
+        })
+
+        if (stickerAdd) {
+          ctx.session.user.stickerSet.create = true
+          ctx.session.user.stickerSet.save()
+          ctx.session.user.save()
         }
-      })
+      } else {
+        stickerAdd = await ctx.telegram.addStickerToSet(ctx.from.id, ctx.session.user.stickerSet.name, {
+          png_sticker: { source: tmpPath },
+          emojis
+        }).catch((error) => {
+          return {
+            error: {
+              telegram: error
+            }
+          }
+        })
+      }
 
       if (stickerAdd) {
-        ctx.session.user.stickerSet.create = true
-        ctx.session.user.stickerSet.save()
-        ctx.session.user.save()
+        const getStickerSet = await ctx.telegram.getStickerSet(ctx.session.user.stickerSet.name)
+        const stickerInfo = getStickerSet.stickers.slice(-1)[0]
+
+        ctx.db.Sticker.addSticker(ctx.session.user.stickerSet.id, emojis, hash, stickerInfo, stickerFile).catch((error) => {
+          return {
+            error: {
+              telegram: error
+            }
+          }
+        })
+
+        return {
+          ok: {
+            title: ctx.session.user.stickerSet.title,
+            link: `${ctx.config.stickerLinkPrefix}${ctx.session.user.stickerSet.name}`,
+            stickerInfo
+          }
+        }
       }
     } else {
-      stickerAdd = await ctx.telegram.addStickerToSet(ctx.from.id, ctx.session.user.stickerSet.name, {
-        png_sticker: { source: tmpPath },
-        emojis
-      }).catch((error) => {
-        return {
-          error: {
-            telegram: error
-          }
-        }
-      })
-    }
-
-    if (stickerAdd) {
-      const getStickerSet = await ctx.telegram.getStickerSet(ctx.session.user.stickerSet.name)
-      const stickerInfo = getStickerSet.stickers.slice(-1)[0]
-
-      ctx.db.Sticker.addSticker(ctx.session.user.stickerSet.id, emojis, hash, stickerInfo, stickerFile).catch((error) => {
-        return {
-          error: {
-            telegram: error
-          }
-        }
-      })
-
       return {
-        ok: {
-          title: ctx.session.user.stickerSet.title,
-          link: `${ctx.config.stickerLinkPrefix}${ctx.session.user.stickerSet.name}`,
-          stickerInfo
-        }
+        error: 'ITS_ANIMATED'
       }
     }
+  })()
 
-    try {
-      fs.unlinkSync(tmpPath)
-    } catch (error) {
-      console.error(error)
-    }
-  } else {
-    return {
-      error: 'ITS_ANIMATED'
-    }
+  try {
+    fs.unlinkSync(tmpPath)
+  } catch (error) {
+    console.error(error)
   }
+
+  return result
 }
