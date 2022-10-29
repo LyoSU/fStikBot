76605c0540636b2b39771b5630f8dfc4ead87a60
Yuri Ly <mail@lyo.su>
2022-10-30 01:56:26 +0300

    0.32.2 hide catalog offer

--- handlers/sticker.js
+++ handlers/sticker.js
@@ -126,25 +126,25 @@ module.exports = async (ctx) => {
         messageText = result.messageText
         replyMarkup = result.replyMarkup
 
-        if (typeof stickerSet?.publishDate === 'undefined' && !stickerSet?.animated && !stickerSet?.inline) {
-          const countStickers = await ctx.db.Sticker.count({
-            stickerSet,
-            deleted: false
-          })
+        // if (typeof stickerSet?.publishDate === 'undefined' && !stickerSet?.animated && !stickerSet?.inline) {
+        //   const countStickers = await ctx.db.Sticker.count({
+        //     stickerSet,
+        //     deleted: false
+        //   })
 
-          if ([15, 50, 80, 120].includes(countStickers)) {
-            setTimeout(async () => {
-              await ctx.replyWithHTML(ctx.i18n.t('sticker.add.catalog_offer', {
-                title: escapeHTML(stickerSet.title),
-                link: `${ctx.config.stickerLinkPrefix}${stickerSet.name}`
-              }), {
-                reply_markup: Markup.inlineKeyboard([
-                  Markup.callbackButton(ctx.i18n.t('callback.pack.btn.catalog_add'), `catalog:publish:${stickerSet.id}`)
-                ])
-              })
-            }, 1000 * 2)
-          }
-        }
+        //   if ([15, 50, 80, 120].includes(countStickers)) {
+        //     setTimeout(async () => {
+        //       await ctx.replyWithHTML(ctx.i18n.t('sticker.add.catalog_offer', {
+        //         title: escapeHTML(stickerSet.title),
+        //         link: `${ctx.config.stickerLinkPrefix}${stickerSet.name}`
+        //       }), {
+        //         reply_markup: Markup.inlineKeyboard([
+        //           Markup.callbackButton(ctx.i18n.t('callback.pack.btn.catalog_add'), `catalog:publish:${stickerSet.id}`)
+        //         ])
+        //       })
+        //     }, 1000 * 2)
+        //   }
+        // }
       } else {
         ctx.session.previousSticker = {
           file: stickerFile
