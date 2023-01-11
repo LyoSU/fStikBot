9a485a40ec322584434ff128a5c86a9b73ba7c2d
Yuri Ly <mail@lyo.su>
2023-01-11 20:12:02 +0200

    0.35.0 send original update (convert sticker)

--- scenes/sricker-original.js
+++ scenes/sricker-original.js
@@ -1,5 +1,7 @@
 const Scene = require('telegraf/scenes/base')
 const Markup = require('telegraf/markup')
+const got = require('got')
+const sharp = require('sharp')
 
 const originalSticker = new Scene('originalSticker')
 
@@ -44,9 +46,42 @@ originalSticker.on('sticker', async (ctx) => {
       }
     })
   } else {
-    await ctx.replyWithHTML(ctx.i18n.t('scenes.original.error.not_found'), {
-      reply_to_message_id: ctx.message.message_id
-    })
+    const fileLink = await ctx.telegram.getFileLink(ctx.message.sticker.file_id)
+
+    if (fileLink.endsWith('.webp')) {
+      const buffer = await got(fileLink).buffer()
+      const image = await sharp(buffer).png()
+
+      await ctx.replyWithDocument({
+        source: image,
+        filename: `${ctx.message.sticker.file_unique_id}.png`
+      }, {
+        reply_to_message_id: ctx.message.message_id
+      }).catch((error) => {
+        ctx.replyWithHTML(ctx.i18n.t('error.telegram', {
+          error: error.description
+        }), {
+          reply_to_message_id: ctx.message.message_id
+        })
+      })
+    } else if (fileLink.endsWith('.webm')) {
+      await ctx.replyWithDocument({
+        url: fileLink,
+        filename: `${ctx.message.sticker.file_unique_id}.webm`
+      }, {
+        reply_to_message_id: ctx.message.message_id
+      }).catch((error) => {
+        ctx.replyWithHTML(ctx.i18n.t('error.telegram', {
+          error: error.description
+        }), {
+          reply_to_message_id: ctx.message.message_id
+        })
+      })
+    } else {
+      await ctx.replyWithHTML(ctx.i18n.t('scenes.original.error.not_found'), {
+        reply_to_message_id: ctx.message.message_id
+      })
+    }
   }
 })
 
