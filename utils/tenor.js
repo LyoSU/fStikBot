017c4f6dbbb7e9d8418990c4a18908aa9b400b68
Yuri Ly <mail@lyo.su>
2023-02-11 22:49:37 +0200

    0.35.1 set tenor api timeout

--- utils/tenor.js
+++ utils/tenor.js
@@ -1,13 +1,31 @@
 const got = require('got')
 
 const search = async (query, limit, pos) => {
-  const response = await got.get(`https://g.tenor.com/v1/search?q=${query}&key=${process.env.TENOR_KEY}&limit=${limit}&pos=${pos}&searchfilter=sticker`)
+  const response = await got.get(`https://g.tenor.com/v1/search?q=${query}&key=${process.env.TENOR_KEY}&limit=${limit}&pos=${pos}&searchfilter=sticker`, {
+    timeout: {
+      lookup: 100,
+      connect: 50,
+      secureConnect: 50,
+      socket: 1000,
+      send: 10000,
+      response: 1000
+    }
+  })
 
   return JSON.parse(response.body)
 }
 
 const trending = async (pos, locale) => {
-  const response = await got.get(`https://g.tenor.com/v1/trending?key=${process.env.TENOR_KEY}&locale=${locale}&limit=50${pos ? `&pos=${pos}` : ''}&searchfilter=sticker`)
+  const response = await got.get(`https://g.tenor.com/v1/trending?key=${process.env.TENOR_KEY}&locale=${locale}&limit=50${pos ? `&pos=${pos}` : ''}&searchfilter=sticker`, {
+    timeout: {
+      lookup: 100,
+      connect: 50,
+      secureConnect: 50,
+      socket: 1000,
+      send: 10000,
+      response: 1000
+    }
+  })
 
   return JSON.parse(response.body)
 }
