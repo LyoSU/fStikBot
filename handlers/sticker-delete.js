3be8612f069afe418123c24e0cae7ba12dbc1df3
Yuri Ly <mail@lyo.su>
2023-03-14 14:15:54 +0200

    1.5.9 fix delete emoji from set

--- handlers/sticker-delete.js
+++ handlers/sticker-delete.js
@@ -3,63 +3,104 @@ const Markup = require('telegraf/markup')
 module.exports = async (ctx) => {
   let packBotUsername
   let deleteSticker
+  let sticker
 
-  if (ctx.callbackQuery.message.reply_to_message && ctx.callbackQuery.message.reply_to_message.sticker) {
-    const setName = ctx.callbackQuery.message.reply_to_message.sticker.set_name
+  if (!ctx.session.userInfo) ctx.session.userInfo = await ctx.db.User.getData(ctx.from)
 
-    if (setName) {
-      packBotUsername = setName.split('_').pop(-1)
+  const { message } = ctx.callbackQuery
+
+  sticker = await ctx.db.Sticker.findOne({
+    fileUniqueId: ctx.match[2]
+  }).populate('stickerSet')
+
+  if(!sticker) {
+    let setName
+
+    const { reply_to_message } = message
+
+    if (message.reply_to_message.sticker) {
+      setName = reply_to_message.sticker.set_name
+
+      deleteSticker = reply_to_message.sticker.file_id
+    } else if (reply_to_message.entities && reply_to_message.entities[0] && reply_to_message.entities[0].type === 'custom_emoji') {
+      const customEmoji = reply_to_message.entities.find((e) => e.type === 'custom_emoji')
+
+      if (!customEmoji) return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
+
+      const emojiStickers = await ctx.telegram.callApi('getCustomEmojiStickers', {
+        custom_emoji_ids: [customEmoji.custom_emoji_id]
+      })
+
+      if (!emojiStickers) return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
+
+      setName = emojiStickers[0].set_name
+      deleteSticker = emojiStickers[0].file_id
+    }
+
+    if (!setName) {
+      return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
+    }
+
+    packBotUsername = setName.split('_').pop(-1)
+
+    if (!message.reply_to_message || !packBotUsername || packBotUsername !== ctx.options.username) {
+      return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
+    }
+
+    const stickerSet = await ctx.db.StickerSet.findOne({
+      name: setName,
+      owner: ctx.session.userInfo.id
+    })
+
+    if (!stickerSet) {
+      return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
+    }
+
+  } else {
+    if (sticker.stickerSet.owner.toString() === ctx.session.userInfo.id.toString()) {
+      deleteSticker = sticker.info.file_id
     } else {
+      return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
+    }
+
+    deleteSticker = sticker.info.file_id
+  }
+
+  if (!deleteSticker) {
+    return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
+  }
+
+  let deleteStickerFromSet
+  if (ctx.session?.userInfo?.stickerSet?.passcode === 'public') {
+    const stickerSet = await ctx.tg.getStickerSet(sticker.stickerSet.name)
+
+    if (stickerSet.stickers[0].file_unique_id === sticker.fileUniqueId) {
       return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
     }
   }
 
-  if (!ctx.session.userInfo) ctx.session.userInfo = await ctx.db.User.getData(ctx.from)
-  const sticker = await ctx.db.Sticker.findOne({
-    fileUniqueId: ctx.match[2]
-  }).populate('stickerSet')
-
-  if (ctx.callbackQuery.message.reply_to_message && packBotUsername && packBotUsername === ctx.options.username) {
-    deleteSticker = ctx.callbackQuery.message.reply_to_message.sticker.file_id
-  } else if (sticker && sticker.stickerSet.owner.toString() === ctx.session.userInfo.id.toString()) {
-    deleteSticker = sticker.info.file_id
+  if (ctx.session?.userInfo?.stickerSet?.inline) {
+    deleteStickerFromSet = true
   } else {
-    return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
+    deleteStickerFromSet = await ctx.deleteStickerFromSet(deleteSticker).catch((error) => {
+      ctx.answerCbQuery(ctx.i18n.t('error.answerCbQuery.telegram', {
+        error: error.description
+      }), true)
+    })
   }
 
-  if (deleteSticker) {
-    let deleteStickerFromSet
-    if (ctx.session?.userInfo?.stickerSet?.passcode === 'public') {
-      const stickerSet = await ctx.tg.getStickerSet(sticker.stickerSet.name)
+  if (deleteStickerFromSet) {
+    ctx.answerCbQuery(ctx.i18n.t('callback.sticker.answerCbQuery.delete'))
 
-      if (stickerSet.stickers[0].file_unique_id === sticker.fileUniqueId) {
-        return ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
-      }
+    ctx.editMessageText(ctx.i18n.t('callback.sticker.delete'), {
+      reply_markup: Markup.inlineKeyboard([
+        Markup.callbackButton(ctx.i18n.t('callback.sticker.btn.restore'), `restore_sticker:${sticker?.info?.file_unique_id}`, !sticker?.info)
+      ])
+    }).catch(() => {})
+
+    if (sticker) {
+      sticker.deleted = true
+      await sticker.save()
     }
-
-    if (ctx.session.userInfo.stickerSet && ctx.session.userInfo.stickerSet.inline) {
-      deleteStickerFromSet = true
-    } else {
-      deleteStickerFromSet = await ctx.deleteStickerFromSet(deleteSticker).catch((error) => {
-        ctx.answerCbQuery(ctx.i18n.t('error.answerCbQuery.telegram', {
-          error: error.description
-        }), true)
-      })
-    }
-
-    if (deleteStickerFromSet) {
-      ctx.answerCbQuery(ctx.i18n.t('callback.sticker.answerCbQuery.delete'))
-
-      ctx.editMessageText(ctx.i18n.t('callback.sticker.delete'), {
-        reply_markup: Markup.inlineKeyboard([
-          Markup.callbackButton(ctx.i18n.t('callback.sticker.btn.restore'), `restore_sticker:${sticker?.info?.file_unique_id}`, !sticker?.info)
-        ])
-      }).catch(() => {})
-
-      if (sticker) {
-        sticker.deleted = true
-        await sticker.save()
-      }
-    }
-  } else ctx.answerCbQuery(ctx.i18n.t('callback.sticker.error.not_found'), true)
+  }
 }
