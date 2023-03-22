ef62974c6c8f6dcbfa08a967f9b83ad70653dde1
Yuri Ly <yuri@lyubchak.com>
2023-03-23 01:09:23 +0200

    1.7.0 - catalog message pin - globally set commands for all private chats by language

--- handlers/start.js
+++ handlers/start.js
@@ -2,52 +2,13 @@ const Markup = require('telegraf/markup')
 const { userName } = require('../utils')
 
 module.exports = async (ctx) => {
-  const commands = [
-    { command: 'start', description: ctx.i18n.t('cmd.start.commands.start') },
-    { command: 'packs', description: ctx.i18n.t('cmd.start.commands.packs') },
-    { command: 'delete', description: ctx.i18n.t('cmd.start.commands.delete') },
-    { command: 'catalog', description: ctx.i18n.t('cmd.start.commands.catalog') },
-    { command: 'publish', description: ctx.i18n.t('cmd.start.commands.publish') },
-    { command: 'lang', description: ctx.i18n.t('cmd.start.commands.lang') },
-    { command: 'donate', description: ctx.i18n.t('cmd.start.commands.donate') }
-  ]
-
-  const myCommands = await ctx.telegram.callApi('getMyCommands', {
-    scope: JSON.stringify({
-      type: 'chat',
-      chat_id: ctx.chat.id
-    })
-  })
-
-  // compare
-  let needUpdate = false
-  if (myCommands.length !== commands.length) {
-    needUpdate = true
-  } else {
-    for (let i = 0; i < commands.length; i++) {
-      const myCommand = myCommands.find(c => c.command === commands[i].command)
-      if (!myCommand || myCommand.description !== commands[i].description) {
-        needUpdate = true
-        break
-      }
-    }
+  if (ctx.chat.type === 'private' && ctx.from.is_bot) {
+    return ctx.deleteMessage()
   }
 
-  if (needUpdate) {
-    await ctx.telegram.callApi('setMyCommands', {
-      commands: JSON.stringify(commands),
-      scope: JSON.stringify({
-        type: 'chat',
-        chat_id: ctx.chat.id
-      })
-    })
-  }
+  const chat = await ctx.getChat()
 
-  await ctx.replyWithHTML(ctx.i18n.t('cmd.start.info', {
-    name: userName(ctx.from)
-  }), Markup.removeKeyboard().extra({ disable_web_page_preview: true }))
-
-  if (ctx.config.catalogUrl) {
+  if (!chat?.pinned_message && ctx.config.catalogUrl) {
     if (ctx.startPayload === 'catalog') {
       await ctx.replyWithHTML(ctx.i18n.t('cmd.start.catalog'), {
         reply_markup: JSON.stringify({
@@ -80,19 +41,18 @@ module.exports = async (ctx) => {
         })
       })
     } else {
-      await ctx.replyWithHTML('👇', {
+      const pinMessage = await ctx.replyWithHTML('⁠&#8288;', {
+        parse_mode: 'HTML',
         reply_markup: JSON.stringify({
           inline_keyboard: [
             [
-              {
-                text: ctx.i18n.t('cmd.start.btn.catalog_mini'),
-                web_app: {
-                  url: ctx.config.catalogUrl,
-                  request_write_access: true
-                }
-              }
-            ],
-            [
+              // {
+              //   text: ctx.i18n.t('cmd.start.btn.catalog_mini'),
+              //   web_app: {
+              //     url: ctx.config.catalogUrl,
+              //     request_write_access: true
+              //   }
+              // },
               {
                 text: ctx.i18n.t('cmd.start.btn.catalog_app_mini'),
                 url: ctx.config.catalogAppUrl
@@ -110,6 +70,14 @@ module.exports = async (ctx) => {
           ]
         })
       })
+
+      await ctx.pinChatMessage(pinMessage.message_id, {
+        disable_notification: true
+      })
     }
   }
+
+  await ctx.replyWithHTML(ctx.i18n.t('cmd.start.info', {
+    name: userName(ctx.from)
+  }), Markup.removeKeyboard().extra({ disable_web_page_preview: true }))
 }
