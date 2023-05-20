2a7fab575b9851b695928f2542fda2065a263a82
Yuri Ly <yuri@lyubchak.com>
2023-05-20 22:30:11 +0300

    1.17.7 change show news channel promote

--- handlers/news-channel.js
+++ handlers/news-channel.js
@@ -6,55 +6,59 @@ const composer = new Composer()
 
 composer.on('message', Composer.optional((ctx) => ctx?.chat?.type === 'private', async (ctx, next) => {
   // if ru locale
-  if (ctx.session.userInfo.locale === 'ru' || ctx.from.language_code === 'ru') {
-    if (!ctx.config.ruNewsChannel.id) return next()
+  if (ctx.session.userInfo.locale !== 'ru' || ctx.from.language_code !== 'ru') {
+    return next()
+  }
 
-    // if not command
-    if (ctx.message.text && ctx.message.text.indexOf('/') === 0) return next()
+  if (!ctx.config.ruNewsChannel.id) return next()
 
-    // if createdAt < 14 days
-    if (ctx.session.userInfo.createdAt > new Date().getTime() - 1000 * 60 * 60 * 24 * 14) {
-      return next()
+  // if not command
+  if (ctx.message.text && ctx.message.text.indexOf('/') === 0) return next()
+
+  // if createdAt < 14 days
+  if (ctx.session.userInfo.createdAt > new Date().getTime() - 1000 * 60 * 60 * 24 * 14) {
+    return next()
+  }
+
+  if (ctx.session.userInfo.newsSubscribedDate > new Date().getTime() - 1000 * 60 * 60 * 24 * 7) {
+    return next()
+  }
+
+  if (Math.random() > 0.01) {
+    return next()
+  }
+
+  // check subscribe to channel
+  const getChatMember = await ctx.telegram.getChatMember(ctx.config.ruNewsChannel.id, ctx.from.id).catch((error) => {
+    console.error('getChatMember error', error)
+    return {
+      status: 'error',
+      error
     }
+  })
 
-    if (ctx.session.userInfo.newsSubscribedDate > new Date().getTime() - 1000 * 60 * 60 * 24 * 7) {
-      return next()
-    }
-
-    // check subscribe to channel
-    const getChatMember = await ctx.telegram.getChatMember(ctx.config.ruNewsChannel.id, ctx.from.id).catch((error) => {
-      console.error('getChatMember error', error)
-      return {
-        status: 'error',
-        error
+  if (['member', 'administrator', 'creator'].indexOf(getChatMember.status) === -1) {
+    await ctx.replyWithHTML(ctx.i18n.t('news.join', {
+      link: ctx.config.ruNewsChannel.link
+    }), {
+      disable_web_page_preview: true,
+      reply_markup: {
+        inline_keyboard: [
+          [{
+            text: ctx.i18n.t('news.join_btn'),
+            url: ctx.config.ruNewsChannel.link
+          }],
+          [{
+            text: ctx.i18n.t('news.continue'),
+            callback_data: 'start'
+          }]
+        ]
       }
     })
 
-    if (['member', 'administrator', 'creator'].indexOf(getChatMember.status) === -1) {
-      await ctx.replyWithHTML(ctx.i18n.t('news.join', {
-        link: ctx.config.ruNewsChannel.link
-      }), {
-        disable_web_page_preview: true,
-        reply_markup: {
-          inline_keyboard: [
-            [{
-              text: ctx.i18n.t('news.join_btn'),
-              url: ctx.config.ruNewsChannel.link
-            }],
-            [{
-              text: ctx.i18n.t('news.continue'),
-              callback_data: 'start'
-            }]
-          ]
-        }
-      })
-
-      return next()
-    } else {
-      ctx.session.userInfo.newsSubscribedDate = new Date()
-      return next()
-    }
+    return next()
   } else {
+    ctx.session.userInfo.newsSubscribedDate = new Date()
     return next()
   }
 }))
