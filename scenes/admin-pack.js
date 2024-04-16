95fc6359af0552f118e29dc3810bab0bd3f971d9
Yuri Ly <yuri@lyubchak.com>
2024-04-16 14:25:56 +0300

    Fix null check in news-channel.js

--- scenes/admin-pack.js
+++ scenes/admin-pack.js
@@ -37,7 +37,7 @@ adminPackFind.on(['sticker', 'text'], async (ctx) => {
 
 
   if (packName.split('_').pop(-1) !== ctx.options.username) {
-    return 'This sticker pack is not from this bot'
+    return ctx.reply('This sticker pack is not from this bot')
   }
 
   const stickerSet = await ctx.tg.getStickerSet(packName)
@@ -48,7 +48,7 @@ adminPackFind.on(['sticker', 'text'], async (ctx) => {
 
 
   if (!stickerSet) {
-    return 'Sticker pack not found'
+    return ctx.reply('This sticker pack does not exist')
   }
 
   ctx.session.admin = {
