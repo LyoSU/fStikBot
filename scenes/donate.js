2d958c583845129964548e7d02be197aee089f8c
Yuri Ly <yuri@lyubchak.com>
2024-06-06 21:21:29 +0300

    Update donate scene

--- scenes/donate.js
+++ scenes/donate.js
@@ -27,6 +27,7 @@ const donate = async (ctx) => {
   }
 
   const price = amount / 5
+  const starPrice = amount * 5
   const priceUAH = (price * exchangeRate.UAH).toFixed(2)
   const priceRUB = (price * exchangeRate.RUB).toFixed(2)
 
@@ -94,8 +95,6 @@ const donate = async (ctx) => {
   })
 
   await telegramPayment.save()
-
-  const starPrice = amount * 5
 
   const replyMarkup =  Markup.inlineKeyboard([
     [Markup.payButton(`⭐️ Telegram Stars`)],
