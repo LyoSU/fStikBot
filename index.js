088b3de3933072914445614773534152d365e087
Yuri Ly <yuri@lyubchak.com>
2024-10-18 16:10:01 +0300

    disable jobs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: './.env' })
 require('./bot')
 
-require('./update-packs')
-require('./top-sets')
-require('./payment-giver')
+// require('./update-packs')
+// require('./payment-giver')
+// require('./top-sets')
