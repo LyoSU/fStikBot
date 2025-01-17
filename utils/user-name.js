cbf319ef125fea04737e200803848383d375620e
Yuri Ly <yuri@lyubchak.com>
2025-01-17 15:56:45 +0200

    feat | Escape ampersands in user names for improved HTML safety

--- utils/user-name.js
+++ utils/user-name.js
@@ -2,7 +2,7 @@ module.exports = (user, url = false) => {
   let name = user.first_name
 
   if (user.last_name) name += ` ${user.last_name}`
-  name = name.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  name = name.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
 
   if (url) return `<a href="tg://user?id=${user.id}">${name}</a>`
   return name
