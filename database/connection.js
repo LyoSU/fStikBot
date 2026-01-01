7e821867862f1f6e7bb428cd9dc56d984bd510a9
Yuri Ly <yuri@lyubchak.com>
2026-01-01 22:51:37 +0200

    feat: Ensure direct connection is explicitly set for both MongoDB and Atlas connections

--- database/connection.js
+++ database/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 const connection = mongoose.createConnection(process.env.MONGODB_URI, {
+  directConnection: true,
   maxPoolSize: 50,
   maxTimeMS: 30000,
   serverSelectionTimeoutMS: 5000,
   socketTimeoutMS: 45000,
   maxIdleTimeMS: 30000,
   useNewUrlParser: true,
-
   useUnifiedTopology: true
 })
 
@@ -17,6 +17,7 @@ connection.on('error', error => {
 })
 
 const atlasConnection = mongoose.createConnection(process.env.ATLAS_MONGODB_URI || process.env.MONGODB_URI, {
+  directConnection: true,
   maxPoolSize: 50,
   maxTimeMS: 30000,
   serverSelectionTimeoutMS: 5000,
